c2d5031172c54f3610a56f5bcdd789325498e80a
D V <divyadityasnaruka@gmail.com>
2023-12-29 15:10:28 +0530

    reset custom workflow

--- src/options/src/index.ts
+++
@@ -1,9 +0,0 @@
-import ForgeDOM from '../../common/ForgeDOM'
-import App from './App'
-
-import './index.css'
-
-const root = document.getElementById('root') as HTMLDivElement
-
-root.attach(App)
-console.log(ForgeDOM.parserCount)
