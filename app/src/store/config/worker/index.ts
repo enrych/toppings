9f36c0be19ab2879cf32c6078019df853e843fa2
D V <divyadityasnaruka@gmail.com>
2024-07-05 18:03:03 +0530

    store exports WorkerName Type

--- app/src/store/config/worker/index.ts
+++ app/src/store/config/worker/index.ts
@@ -1,13 +1,17 @@
 import youtubeWorkerConfig, {
-	type YouTubeWorkerConfig,
-} from '../../../modules/content_scripts/workers/youtube/config';
+  type YouTubeWorkerConfig,
+} from "../../../modules/content_scripts/workers/youtube/config";
 import udemyWorkerConfig, {
-	UdemyWorkerConfig,
-} from '../../../modules/content_scripts/workers/udemy/config';
+  UdemyWorkerConfig,
+} from "../../../modules/content_scripts/workers/udemy/config";
 
 export type { YouTubeWorkerConfig, UdemyWorkerConfig };
 
-export default {
-	youtube: youtubeWorkerConfig,
-	udemy: udemyWorkerConfig,
+const workersConfig = {
+  youtube: youtubeWorkerConfig,
+  udemy: udemyWorkerConfig,
 };
+
+export type WorkerName = keyof typeof workersConfig;
+
+export default workersConfig;
