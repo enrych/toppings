2a33784af992241d41effa83d5b05152a3ce47f1
D V <divyadityasnaruka@gmail.com>
2024-07-07 01:27:21 +0530

    fix options isLoading svg

--- app/src/modules/options/src/components/PreferenceInput.tsx
+++ app/src/modules/options/src/components/PreferenceInput.tsx
@@ -122,7 +122,26 @@ export default function PreferenceInput({
             onChange={preferenceChangeHandler}
           />
           {isLoading && (
-            <div className="absolute top-1/2 -translate-y-1/2 -right-6 animate-spin rounded-full h-5 w-5 border-t-2 border-blue-500"></div>
+            <svg
+              className="absolute top-[8px] -right-6 animate-spin h-5 w-5 text-blue-500"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              />
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291l-1.707-1.707A7.968 7.968 0 014 12H0c0 3.312 1.344 6.312 3.515 8.485l2.485-2.485z"
+              />
+            </svg>
           )}
           {isValid === true && (
             <svg
