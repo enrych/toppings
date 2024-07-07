1735019c417c3f9d9eaf3ed428e944c33c52ff48
D V <divyadityasnaruka@gmail.com>
2024-07-07 23:44:10 +0530

    add shadcn to docs

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background antialiased",
+          inter.className,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
