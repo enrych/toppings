32c3382f822415527845ceb101f8f6acc5325aab
D V <divyadityasnaruka@gmail.com>
2024-07-08 16:35:31 +0530

    add AddToBrowser

---
+++ docs/src/components/AddToBrowser.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import getExtensionWebStore from "@/lib/getExtensionWebStore";
+import { Button } from "./ui/button";
+
+const AddToBrowser = () => {
+  const { isSupported, browser, store } = getExtensionWebStore();
+  return (
+    <Button className="px-6 py-6 gap-2" asChild>
+      <Link href={store} target="_blank">
+        <Image
+          src={`/assets/icons/${isSupported ? browser : "toppings"}.svg`}
+          alt={`${browser} Icon`}
+          width={24}
+          height={24}
+        />
+        Add to{" "}
+        {isSupported
+          ? browser.at(0)?.toUpperCase() + browser.slice(1)
+          : "Browser"}
+      </Link>
+    </Button>
+  );
+};
+
+export default AddToBrowser;
