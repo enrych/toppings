af6afb7b9459ea9f559f6a9176d0d75f7c237585
D V <divyadityasnaruka@gmail.com>
2024-07-08 12:15:52 +0530

    refactor google fonts logic docs

---
+++ docs/src/app/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Akronim } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const akronim = Akronim({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { inter } from "./fonts";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Toppings: Your web, your way",
