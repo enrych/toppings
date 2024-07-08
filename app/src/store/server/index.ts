3c8cc5b97d05a2751b36b765cfa2674607e5677f
D V <divyadityasnaruka@gmail.com>
2024-07-09 03:35:10 +0530

    update server base link to render

--- app/src/store/server/index.ts
+++ app/src/store/server/index.ts
@@ -1,6 +1,6 @@
 const SERVER_BASE_URI =
-	process.env.NODE_ENV === 'development'
-		? 'http://localhost:8000'
-		: 'https://toppings.pythonanywhere.com/v1';
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000"
+    : "https://toppings.onrender.com";
 
 export default SERVER_BASE_URI;
--- app/test/unit/store/server/index.spec.ts
+++ app/test/unit/store/server/index.spec.ts
@@ -3,6 +3,6 @@ import { SERVER_BASE_URI } from "../../../../src/store";
 
 describe("SERVER_BASE_URI", () => {
   it("should use correct server URI", () => {
-    expect(SERVER_BASE_URI).to.equal("https://toppings.pythonanywhere.com/v1");
+    expect(SERVER_BASE_URI).to.equal("https://toppings.onrender.com");
   });
 });
