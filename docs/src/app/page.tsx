b58121a5f0a124eba9e095cf4ddbc71426ceea25
D V <divyadityasnaruka@gmail.com>
2024-07-08 21:51:44 +0530

    add initial home page to docs

--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -1,9 +1,34 @@
 import Image from "next/image";
+import heroIllustration from "../../public/assets/illustrations/hero.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Toppings
-    </main>
+    <div className="relative mx-auto flex justify-center items-center w-screen h-auto">
+      <div className="w-full overflow-x-hidden lg:flex flex-col-reverse lg:flex-row justify-around items-center h-full">
+        <div className="flex flex-col items-center justify-center p-24 w-full lg:w-1/2 h-full overflow-x-hidden text-wrap break-words">
+          <h1
+            className="text-[70px] max-w-[500px] font-bold text-gray-900 leading-tight text-center lg:text-left"
+            style={{ fontVariationSettings: "'wght' 900" }}
+          >
+            Your Web,
+            <br />
+            <span className="text-accent">Your Way</span>
+          </h1>
+          <p className="mt-4 text-lg text-gray-700 text-center">
+            Unlock a new level of customization, seamlessly enhance your
+            favorite websites, and enjoy a more personalized browsing
+            experience.
+          </p>
+        </div>
+        <div className="w-full lg:w-1/2">
+          <Image
+            src={heroIllustration}
+            alt="hero"
+            className="max-w-none w-full lg:w-[70vw] relative lg:transform drop-shadow-md select-none"
+            draggable={false}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
