47ed44b7028f33ef0a1eeddf32f5de6778f92c3c
D V <divyadityasnaruka@gmail.com>
2024-07-10 21:54:30 +0530

    fix-dev-server-port

--- app/src/store/server/index.ts
+++ app/src/store/server/index.ts
@@ -1,6 +1,6 @@
 const SERVER_BASE_URI =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:8000"
+    ? "http://localhost:8080"
     : "https://toppings.onrender.com";
 
 export default SERVER_BASE_URI;
