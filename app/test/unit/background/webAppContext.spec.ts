27453281a8912d8c964aac727fc54c8b46a6fce3
D V <divyadityasnaruka@gmail.com>
2024-09-26 03:31:08 +0530

    fix failing tests

--- app/test/unit/background/webAppContext.spec.ts
+++ app/test/unit/background/webAppContext.spec.ts
@@ -8,16 +8,42 @@ import youtubeConfig from "../../../src/content_scripts/webApps/youtube/webApp.c
 
 describe("getWebAppContext", () => {
   describe("YouTube Parser", () => {
-    const chromeMock = {
-      storage: {
-        sync: {
-          get: sinon.stub().yields({ webApps: { youtube: youtubeConfig } }),
+    let chromeMock: any;
+    let fetchStub: sinon.SinonStub;
+
+    beforeEach(() => {
+      chromeMock = {
+        storage: {
+          sync: {
+            get: sinon.stub().yields({ webApps: { youtube: youtubeConfig } }),
+          },
         },
-      },
-    };
-    global.chrome = chromeMock as any;
+      };
+      global.chrome = chromeMock as any;
+
+      fetchStub = sinon.stub(global, "fetch");
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
     it("should return correct info for a supported YouTube playlist URL", async () => {
       const testCase1 = "https://www.youtube.com/playlist?list=PL1234567890";
+
+      fetchStub.resolves({
+        ok: true,
+        status: 200,
+        json: async () => ({
+          data: {
+            num_videos: "50",
+            playlist_id: "PL1234567890",
+            avg_runtime: 300,
+            total_runtime: 15000,
+          },
+        }),
+      });
+
       const expectedResult1: WebAppContext = {
         isSupported: true,
         appName: "youtube",
@@ -25,14 +51,23 @@ describe("getWebAppContext", () => {
         contextData: {
           webAppURL: new URL(testCase1),
           activeRoute: "playlist",
-          payload: { playlistID: "PL1234567890" },
+          payload: {
+            playlistId: "PL1234567890",
+            averageRuntime: 300,
+            totalRuntime: 15000,
+            totalVideos: "50",
+          },
         },
       };
+
       expect(await getWebAppContext(testCase1)).to.deep.equal(expectedResult1);
+      expect(fetchStub.calledOnce).to.be.true;
+      expect(fetchStub.calledWith(sinon.match.string)).to.be.true;
     });
 
     it("should return correct info for a supported YouTube video URL", async () => {
       const testCase2 = "https://www.youtube.com/watch?v=ABCDEFGHIJK";
+
       const expectedResult2: WebAppContext = {
         isSupported: true,
         appName: "youtube",
@@ -45,15 +80,18 @@ describe("getWebAppContext", () => {
           },
         },
       };
+
       expect(await getWebAppContext(testCase2)).to.deep.equal(expectedResult2);
     });
 
     it("should return unsupported for an unsupported URL", async () => {
       const testCase3 = "https://example.com/";
+
       const expectedResult3: WebAppContext = {
         isSupported: false,
         appName: null,
       };
+
       expect(await getWebAppContext(testCase3)).to.deep.equal(expectedResult3);
     });
   });
--- app/test/unit/content_scripts/webApps/youtube/utils/fetchYouTubeToppings.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-import { SERVER_BASE_URI } from "../../../../../../src/content_scripts/webApps/youtube/utils/fetchYouTubeToppings";
-
-describe("SERVER_BASE_URI", () => {
-  it("should use correct server URI", () => {
-    expect(SERVER_BASE_URI).to.equal("https://toppings.onrender.com");
-  });
-});
